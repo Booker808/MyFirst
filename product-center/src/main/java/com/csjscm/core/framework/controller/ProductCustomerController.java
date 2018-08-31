226eda413d3c51a30a433a34f3aac0a18041dd7b
张浩 <zhangh457@csjscm.com>
2018-08-31 15:50:27 +0800

    修复一些商品查询的bug

--- product-center/src/main/java/com/csjscm/core/framework/controller/ProductCustomerController.java
+++ product-center/src/main/java/com/csjscm/core/framework/controller/ProductCustomerController.java
@@ -11,11 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
 @Controller
 @RequestMapping("/product/product")
+@ResponseBody
 public class ProductCustomerController {
     @Autowired
     private ProductCustomerService productCustomerService;
