5070c4638898db334cce4af62bb9330a6b26c17b
张浩 <zhangh457@csjscm.com>
2018-09-07 14:50:45 +0800

    修改skuCore/skuCustomer/skuPartner中的表结构及相应字段

--- modules/dao/src/main/java/com/csjscm/core/framework/model/SkuPartner.java
+++ modules/dao/src/main/java/com/csjscm/core/framework/model/SkuPartner.java
@@ -1,10 +1,12 @@
 package com.csjscm.core.framework.model;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 public class SkuPartner {
     private Integer id;
 
@@ -14,14 +16,17 @@ public class SkuPartner {
     @ApiModelProperty("供应商编码")
     private String supplyNo;
 
+    @ApiModelProperty("供应商商品编码")
+    private String supplyPdNo;
+
     @ApiModelProperty("供应商名称")
-    private String supplyName;
+    private String supplyPdName;
 
     @ApiModelProperty("供应商商品规格")
-    private String supplyRule;
+    private String supplyPdRule;
 
     @ApiModelProperty("供应商商品型号")
-    private String supplySize;
+    private String supplyPdSize;
 
     @ApiModelProperty("品牌ID")
     private String brandId;
@@ -86,276 +91,4 @@ public class SkuPartner {
     private String udf19;
 
     private String udf20;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getProductNo() {
-        return productNo;
-    }
-
-    public void setProductNo(String productNo) {
-        this.productNo = productNo;
-    }
-
-    public String getSupplyNo() {
-        return supplyNo;
-    }
-
-    public void setSupplyNo(String supplyNo) {
-        this.supplyNo = supplyNo;
-    }
-
-    public String getSupplyName() {
-        return supplyName;
-    }
-
-    public void setSupplyName(String supplyName) {
-        this.supplyName = supplyName;
-    }
-
-    public String getSupplyRule() {
-        return supplyRule;
-    }
-
-    public void setSupplyRule(String supplyRule) {
-        this.supplyRule = supplyRule;
-    }
-
-    public String getSupplySize() {
-        return supplySize;
-    }
-
-    public void setSupplySize(String supplySize) {
-        this.supplySize = supplySize;
-    }
-
-    public String getBrandId() {
-        return brandId;
-    }
-
-    public void setBrandId(String brandId) {
-        this.brandId = brandId;
-    }
-
-    public String getBrandName() {
-        return brandName;
-    }
-
-    public void setBrandName(String brandName) {
-        this.brandName = brandName;
-    }
-
-    public BigDecimal getRefrencePrice() {
-        return refrencePrice;
-    }
-
-    public void setRefrencePrice(BigDecimal refrencePrice) {
-        this.refrencePrice = refrencePrice;
-    }
-
-    public BigDecimal getRecentEnquiry() {
-        return recentEnquiry;
-    }
-
-    public void setRecentEnquiry(BigDecimal recentEnquiry) {
-        this.recentEnquiry = recentEnquiry;
-    }
-
-    public String getCreateUserId() {
-        return createUserId;
-    }
-
-    public void setCreateUserId(String createUserId) {
-        this.createUserId = createUserId;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getEditUserId() {
-        return editUserId;
-    }
-
-    public void setEditUserId(String editUserId) {
-        this.editUserId = editUserId;
-    }
-
-    public Date getEditTime() {
-        return editTime;
-    }
-
-    public void setEditTime(Date editTime) {
-        this.editTime = editTime;
-    }
-
-    public String getUdf1() {
-        return udf1;
-    }
-
-    public void setUdf1(String udf1) {
-        this.udf1 = udf1;
-    }
-
-    public String getUdf2() {
-        return udf2;
-    }
-
-    public void setUdf2(String udf2) {
-        this.udf2 = udf2;
-    }
-
-    public String getUdf3() {
-        return udf3;
-    }
-
-    public void setUdf3(String udf3) {
-        this.udf3 = udf3;
-    }
-
-    public String getUdf4() {
-        return udf4;
-    }
-
-    public void setUdf4(String udf4) {
-        this.udf4 = udf4;
-    }
-
-    public String getUdf5() {
-        return udf5;
-    }
-
-    public void setUdf5(String udf5) {
-        this.udf5 = udf5;
-    }
-
-    public String getUdf6() {
-        return udf6;
-    }
-
-    public void setUdf6(String udf6) {
-        this.udf6 = udf6;
-    }
-
-    public String getUdf7() {
-        return udf7;
-    }
-
-    public void setUdf7(String udf7) {
-        this.udf7 = udf7;
-    }
-
-    public String getUdf8() {
-        return udf8;
-    }
-
-    public void setUdf8(String udf8) {
-        this.udf8 = udf8;
-    }
-
-    public String getUdf9() {
-        return udf9;
-    }
-
-    public void setUdf9(String udf9) {
-        this.udf9 = udf9;
-    }
-
-    public String getUdf10() {
-        return udf10;
-    }
-
-    public void setUdf10(String udf10) {
-        this.udf10 = udf10;
-    }
-
-    public String getUdf11() {
-        return udf11;
-    }
-
-    public void setUdf11(String udf11) {
-        this.udf11 = udf11;
-    }
-
-    public String getUdf12() {
-        return udf12;
-    }
-
-    public void setUdf12(String udf12) {
-        this.udf12 = udf12;
-    }
-
-    public String getUdf13() {
-        return udf13;
-    }
-
-    public void setUdf13(String udf13) {
-        this.udf13 = udf13;
-    }
-
-    public String getUdf14() {
-        return udf14;
-    }
-
-    public void setUdf14(String udf14) {
-        this.udf14 = udf14;
-    }
-
-    public String getUdf15() {
-        return udf15;
-    }
-
-    public void setUdf15(String udf15) {
-        this.udf15 = udf15;
-    }
-
-    public String getUdf16() {
-        return udf16;
-    }
-
-    public void setUdf16(String udf16) {
-        this.udf16 = udf16;
-    }
-
-    public String getUdf17() {
-        return udf17;
-    }
-
-    public void setUdf17(String udf17) {
-        this.udf17 = udf17;
-    }
-
-    public String getUdf18() {
-        return udf18;
-    }
-
-    public void setUdf18(String udf18) {
-        this.udf18 = udf18;
-    }
-
-    public String getUdf19() {
-        return udf19;
-    }
-
-    public void setUdf19(String udf19) {
-        this.udf19 = udf19;
-    }
-
-    public String getUdf20() {
-        return udf20;
-    }
-
-    public void setUdf20(String udf20) {
-        this.udf20 = udf20;
-    }
 }
