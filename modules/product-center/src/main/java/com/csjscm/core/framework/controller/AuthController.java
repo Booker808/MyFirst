2eb2b0d6432abb901947c5760f3e961670d601c9
paean <chenyk271@csjscm.com>
2018-10-19 09:35:18 +0800

    配置文件管理-cyk

--- modules/product-center/src/main/java/com/csjscm/core/framework/controller/AuthController.java
+++ modules/product-center/src/main/java/com/csjscm/core/framework/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.csjscm.core.framework.controller;
 
+import com.csjscm.core.framework.service.impl.SkuCoreServiceImpl;
 import com.csjscm.sweet.framework.auth.AuthUtils;
 import com.csjscm.sweet.framework.core.mvc.APIResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/system/auth")
 @ResponseBody
 public class AuthController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @ApiOperation("退出登录")
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
@@ -28,6 +33,9 @@ public class AuthController {
         APIResponse apiResponse=new  APIResponse();
         apiResponse.setData(AuthUtils.getSessionUser());
         apiResponse.setMessage(System.getProperty("sweet.framework.scm.domain"));
+        System.out.println();
+        logger.info("当前用户权限集合："+AuthUtils.getSessionAttribute("SWEET-SESSION-USER_PERMISSION"));
+        System.out.println();
         apiResponse.setCode("success");
         return apiResponse;
     }
